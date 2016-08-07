ddb686813a7f099b9dc75858a3957d5955d4eb25
x14114640 <florinbest90@yahoo.com>
2016-08-07 01:03:42 +0000

    Fixed the problem where cmls file wasnt displayed at the click of a button because I missed a ( in my JS code

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 var http = require('http');
 var path = require('path');
+var express = require('express');
 var bodyParser = require('body-parser');
 var fs = require('fs');
 var js2xmlparser = require("js2xmlparser");
 var xslt = require('node_xslt');
 var async = require('async');
 var socketio = require('socket.io');
-var express = require('express');
+
 
 
 var router = express();
 var server = http.createServer(router);
 var io = socketio.listen(server);
 
-router.use(express.static(path.resolve(__dirname, 'public')));
-router.use(bodyParser.urlencoded({extended: true}));
-router.use(bodyParser.json());
+// router.use(express.static(path.resolve(__dirname, 'client')));
+// router.use(bodyParser.urlencoded({extended: true}));
+// router.use(bodyParser.json());
 
 router.use(express.static(path.resolve(__dirname, 'client')));
 var messages = [];
